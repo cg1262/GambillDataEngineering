098fed4b7ce5866e23c23bfbdd0bcaa6c59dd45f
Chris Gambill <cgambil@wgu.edu>
2024-10-18 10:56:53 -0400

    cg

--- CodeExamples/ExceptionHandling.py
+++ CodeExamples/ExceptionHandling.py
@@ -1,5 +1,25 @@
 import sys 
 
+try:
+    file = open('file.txt', 'r')
+    # Perform file operations
+except FileNotFoundError:
+    print('File not found.')
+except Exception as e:
+    print(e)
+finally:
+    sys.exit(0)
+
+import sys
+ 
+try:
+    value = int(input('Enter a number: '))
+    result = 10 / value
+except ValueError:
+    print("That’s not a valid number!")
+except ZeroDivisionError:
+    print("Oops! You can’t divide by zero.")
+    
 def check_age(age):
     age = int(age)
      
@@ -9,3 +29,5 @@ def check_age(age):
     return True
     
 check_age(input("Please Enter Your Age: "))
+
+print(f"I still moved on!")
